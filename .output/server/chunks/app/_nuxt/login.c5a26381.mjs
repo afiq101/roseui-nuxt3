f4b430c4ac8cf5125530f6f2d2fd6e6b3716c0d2
Syahril Sulaiman <syahrilrevolusi@gmail.com>
2022-11-16 17:45:19 +0800

    teng commit - generate build for prod

--- .output/server/chunks/app/_nuxt/login.c5a26381.mjs
+++
@@ -1,4 +0,0 @@
-const _imports_0 = "" + globalThis.__buildAssetsURL("login.48974946.svg");
-
-export { _imports_0 as _ };
-//# sourceMappingURL=login.c5a26381.mjs.map
--- .output/server/chunks/app/_nuxt/vue-code-highlight-styles.c807dcdd.mjs
+++
@@ -1,6 +0,0 @@
-import { p as prismOkaidia, w as window } from '../styles.mjs';
-
-const vueCodeHighlightStyles_c807dcdd = [prismOkaidia, window];
-
-export { vueCodeHighlightStyles_c807dcdd as default };
-//# sourceMappingURL=vue-code-highlight-styles.c807dcdd.mjs.map
